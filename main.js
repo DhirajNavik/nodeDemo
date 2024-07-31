e3cf15456a368a213dcabc0884301943a7c239de
DhirajNavik <dhirajnavkdk2000@gmail.com>
2024-07-31 17:17:48 +0530

    node_tut

---
+++ main.js
@@ -0,0 +1,12 @@
+var a=1;
+var b=2;
+var ans = a+b;
+console.log(ans);//node main.js
+
+var prompt = require('prompt-sync')();
+const age = prompt("Enter num");
+if(age <18){
+    console.log("-18")
+}else{
+    console.log("+18")
+}
---
+++ notes.js
@@ -0,0 +1,12 @@
+console.log('notes file loaded');
+
+var age= 24;
+
+const addNumbers = function(a,b){
+    return a+b;
+}
+
+module.exports= {
+    age,
+    addNumbers
+}
