be4a3278ef7664610dedbf0a1e7c5238f3f2471f
DhirajNavik <dhirajnavkdk2000@gmail.com>
2024-08-13 02:28:53 +0530

    version 2

--- server.js
+++ server.js
@@ -1,22 +1,23 @@
 const express = require("express");
 const app = express();
 const db = require("./db");
+require("dotenv").config();
 
 const bodyParser = require("body-parser");
 app.use(bodyParser.json());
+const PORT = process.env.PORT || 3000;
 
 
 app.get("/", function (req, res) {
   res.send("Hello world");
 });
 
+const personRoutes = require("./routes/personRoutes");
+const menuRoutes = require("./routes/menuRoutes");
 
-const personRoutes = require("./routes/personRoutes")
-const menuRoutes = require("./routes/menuRoutes")
+app.use("/person", personRoutes);
+app.use("/", menuRoutes);
 
-app.use('/person',personRoutes);
-app.use("/",menuRoutes)
-
-app.listen(3000, () => {
+app.listen(PORT, () => {
   console.log("server is lie on 3000");
 });
